402a59b2eb03a5b6760aa62890e060e004d4a767
Namanpreet Kaur <namangurnaz@gmail.com>
2020-09-18 00:45:44 +0530

    React Last

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Card, CardImg, Breadcrumb, BreadcrumbItem, CardText, CardBody, CardTitle, Button } from 'reactstrap';
+import { Card, CardImg, Breadcrumb, BreadcrumbItem, CardText, CardBody, CardTitle } from 'reactstrap';
 import { Link } from 'react-router-dom';
 import CommentForm from './CommentForm';
 import { Loading } from './LoadingComponent';
